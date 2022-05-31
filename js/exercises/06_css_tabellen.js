b781567878d23f0de14b8ec3613103e7ef520248
dcbu <dennis@dennismail.de>
2022-05-31 08:55:30 +0000

    (c) Fixed some tipps

--- js/exercises/06_css_tabellen.js
+++ js/exercises/06_css_tabellen.js
@@ -1,8 +1,12 @@
-import { elCheckStyleSameValue, cssBorderColorNames, cssBorderWidthNames, cssBorderStyleNames } from '../exercise/validation_helper.js';
-import { Exercise } from '../exercise/exercise_base.js';
+import {
+    elCheckStyleSameValue,
+    cssBorderColorNames,
+    cssBorderWidthNames,
+    cssBorderStyleNames,
+} from "../exercise/validation_helper.js";
+import { Exercise } from "../exercise/exercise_base.js";
 
 let exerciseID = "06_css_tabellen";
-
 
 let instructions = `
 Die Tabelle aus Aufgabe 4 hat noch keine Ränder, daher ist sie schwer als Tabelle zu erkennen.
@@ -11,11 +15,23 @@ Die Tabelle aus Aufgabe 4 hat noch keine Ränder, daher ist sie schwer als Tabel
 <li>Die Tabelle soll eine Kontur erhalten, die 2px stark ist, die Farbe <em>#ff0000</em> sowie den Style <em>solid</em> hat.</li>
 <li>Jede Zelle der Tabelle soll eine Kontur erhalten, die 1px stark ist, die Farbe schwarz sowie den Style <em>solid</em> hat.</li>
 </ol>
-`
+`;
 
 let tips = [
-  // {level: 1, title: "Video: Selektoren", content: `<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/QWnvqFLf3ys" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`, weblinks: ["https://wiki.selfhtml.org/wiki/CSS/Selektoren/Typselektor", "https://wiki.selfhtml.org/wiki/CSS/Selektoren/Klassenselektor"], contentIsHTML: true},
-  {level: 2, title: "Benötigte Elemente und Styles", content: `Du benötigst die folgenden HTML Elemente: 
+    {
+        level: 0,
+        title: "Inline-CSS verwenden",
+        content: `Um CSS inline anzuwenden benötigst du das <em>style</em> Attribut. Der Wert des Attributs sind dann CSS Regeln. Ein Beispiel wäre <em>style="color:red;"</em> um die Schriftfarbe auf rot zu setzen. Die CSS Eigenschaft (color) steht vor dem Doppelpunkt (:), der Wert (red) dahinter, abgeschlossen wird immer mit einem Semikolon (;).  Unten findest du einen Link, der den Aufbau eines solchen style-Attributs genauer beschreibt. Zusatztipp: Da style ein Attribut ist, schreibt man es IM Starttag.
+  `,
+        weblinks: [
+            "https://wiki.selfhtml.org/wiki/CSS/Tutorials/Einstieg/Stylesheets_einbinden#direkt_in_einem_style-Attribut",
+        ],
+        contentIsHTML: true,
+    },
+    {
+        level: 2,
+        title: "Benötigte Elemente und Styles",
+        content: `Du benötigst die folgenden HTML Elemente: 
   <ul>
   <li>Kontur Farbe: border-color</li>
   <li>Farbwerte: z.B. #ff0000 (je zwei Stellen für r, g und b) = rot, alternativ: einfach <em>red</em></li>
@@ -23,8 +39,19 @@ let tips = [
   <li>Kontur Style: border-style</li>
   <li>Alternativ zusammengefasst: border (Siehe Link 1)</li>
   </ul>
-  `, weblinks: ["https://developer.mozilla.org/de/docs/Web/CSS/border", "https://developer.mozilla.org/de/docs/Web/CSS/border-color", "https://developer.mozilla.org/de/docs/Web/CSS/border-style", "https://developer.mozilla.org/de/docs/Web/CSS/border-width"]},
-  {level: 3, title: "Lösung anzeigen", content: `Die Lösung ist: <xmp>
+  `,
+        weblinks: [
+            "https://developer.mozilla.org/de/docs/Web/CSS/border",
+            "https://developer.mozilla.org/de/docs/Web/CSS/border-color",
+            "https://developer.mozilla.org/de/docs/Web/CSS/border-style",
+            "https://developer.mozilla.org/de/docs/Web/CSS/border-width",
+        ],
+        contentIsHTML: true,
+    },
+    {
+        level: 3,
+        title: "Lösung anzeigen",
+        content: `Die Lösung ist: <xmp>
   <table id="tabelle1" style="border: 2px solid #ff0000">
     <tr id="tr_1">
       <th id="td_1_1" style="border: 1px solid #000000">Schüler</th>
@@ -47,22 +74,57 @@ let tips = [
       <td id="td_4_3" style="border: 1px solid #000000">1</td>
     </tr>
   </table> 
-  </xmp>`, contentIsHTML: true}
-]
+  </xmp>`,
+        contentIsHTML: true,
+    },
+];
 
 let validationFuncs = [
-  function() { return elCheckStyleSameValue(`tabelle1`, cssBorderColorNames, "rgb(255, 0, 0)"); },
-  function() { return elCheckStyleSameValue(`tabelle1`, cssBorderWidthNames, "2px"); },
-  function() { return elCheckStyleSameValue(`tabelle1`, cssBorderStyleNames, "solid"); }
-]
+    function () {
+        return elCheckStyleSameValue(
+            `tabelle1`,
+            cssBorderColorNames,
+            "rgb(255, 0, 0)"
+        );
+    },
+    function () {
+        return elCheckStyleSameValue(`tabelle1`, cssBorderWidthNames, "2px");
+    },
+    function () {
+        return elCheckStyleSameValue(`tabelle1`, cssBorderStyleNames, "solid");
+    },
+];
 
-for (let i=1; i<5; i++) {
-  for (let j=1; j<4; j++) {
-    validationFuncs.push(function() { return elCheckStyleSameValue(`td_${i}_${j}`, cssBorderColorNames, "rgb(0, 0, 0)"); })
-    validationFuncs.push(function() { return elCheckStyleSameValue(`td_${i}_${j}`, cssBorderWidthNames, "1px"); })
-    validationFuncs.push(function() { return elCheckStyleSameValue(`td_${i}_${j}`, cssBorderStyleNames, "solid"); })
-  }
+for (let i = 1; i < 5; i++) {
+    for (let j = 1; j < 4; j++) {
+        validationFuncs.push(function () {
+            return elCheckStyleSameValue(
+                `td_${i}_${j}`,
+                cssBorderColorNames,
+                "rgb(0, 0, 0)"
+            );
+        });
+        validationFuncs.push(function () {
+            return elCheckStyleSameValue(
+                `td_${i}_${j}`,
+                cssBorderWidthNames,
+                "1px"
+            );
+        });
+        validationFuncs.push(function () {
+            return elCheckStyleSameValue(
+                `td_${i}_${j}`,
+                cssBorderStyleNames,
+                "solid"
+            );
+        });
+    }
 }
 
-let exerciseBase = new Exercise(exerciseID, instructions, tips, validationFuncs);
+let exerciseBase = new Exercise(
+    exerciseID,
+    instructions,
+    tips,
+    validationFuncs
+);
 window.onload = exerciseBase.init();
--- js/exercises/07_css_grundlagen.js
+++ js/exercises/07_css_grundlagen.js
@@ -1,8 +1,17 @@
-import { elementIsCorrectTag, elHasCorrectStyleValue, elementsExist, checkTableContent, or, hasCorrectStyleValue, hasQuerySelectorCorrectStyleValue, cssContains, hasSelectorStyleValue} from '../exercise/validation_helper.js';
-import { Exercise } from '../exercise/exercise_base.js';
+import {
+    elementIsCorrectTag,
+    elHasCorrectStyleValue,
+    elementsExist,
+    checkTableContent,
+    or,
+    hasCorrectStyleValue,
+    hasQuerySelectorCorrectStyleValue,
+    cssContains,
+    hasSelectorStyleValue,
+} from "../exercise/validation_helper.js";
+import { Exercise } from "../exercise/exercise_base.js";
 
 let exerciseID = "07_css_grundlagen";
-
 
 let instructions = `
 <ol>
@@ -14,36 +23,77 @@ let instructions = `
 <li>Setze die Strichstärke (font-weight) des Links auf 700.</li>
 <li>Füge zum zweiten Paragraphen einen Aussenabstand nach oben von 15px ein.</li>.
 </ol>
-`
+`;
 
 let tips = [
-  {level: 2, title: "Benötigte Elemente und Styles", content: `<ul>
+    {
+        level: 2,
+        title: "Benötigte Elemente und Styles",
+        content: `<ul>
   <li>Hintergrundfarbe: background-color</li>
   <li>Textfarbe: color</li>
   <li>Farbwerte: z.B. #ff0000 (je zwei Stellen für r, g und b) = rot, alternativ: einfach <em>red</em></li>
   <li>Text zentrieren: text-align</li>
   <li>Schriftfamilie: font-family (Fallbacks werden durch Komma getrennt)</li>
   </ul>
-  `, weblinks: []},
-  {level: 3, title: "Lösung anzeigen", content: `Die Lösung ist: <xmp>
+  `,
+        weblinks: [],
+        contentIsHTML: true,
+    },
+    {
+        level: 3,
+        title: "Lösung anzeigen",
+        content: `Die Lösung ist: <xmp>
   <h1 id="h11" style="background-color: #2F4F4F; color: white; text-align: center;">PMHS Blog</h1>
   <p id="p1" style="font-family: Arial, Helvetica, sans-serif;">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet ornare purus, vitae rhoncus purus. Donec quis nibh vulputate nisl maximus tempor. Donec eget pellentesque odio. Phasellus sed nulla et neque semper finibus. Nullam cursus libero quis vehicula ornare. Sed dictum eros eu magna fringilla, id dapibus mi condimentum. Donec euismod mauris ex, id vestibulum mi efficitur sed. Phasellus in lobortis velit, sed iaculis diam. Nunc eget sapien eget lorem aliquet elementum. Maecenas id magna et purus molestie lacinia. Aenean gravida leo ut metus pellentesque sagittis. Sed in nisi ut ex sodales pharetra in eget orci. Ut ex nisi, laoreet nec sodales eu, tincidunt sed risus.
   </p>
   <p id="p2">Hier geht es direkt <a id="a1" href="https://www.pmhs.de" style="color: #ff0000; font-size: 24px; font-weight: 700;">zur PMHS</a>!</p>
-  </xmp>`, contentIsHTML: true}
-]
+  </xmp>`,
+        contentIsHTML: true,
+    },
+];
 
 let validationFuncs = [
-  function() { return elementIsCorrectTag("h11", "h1"); },
-  function() { return hasCorrectStyleValue("h11", "text-align", `center`); },
-  function() { return hasCorrectStyleValue("h11", "background-color", "rgb(47, 79, 79)"); },
-  function() { return hasCorrectStyleValue("h11", "color", "rgb(255, 255, 255)"); },
-  function() { return hasCorrectStyleValue("a1", "color", "rgb(255, 0, 0)"); },
-  function() { return elHasCorrectStyleValue(document.getElementById("p1"), "p1", "font-family", `Arial, Helvetica, sans-serif`); },
-  function() { return hasCorrectStyleValue("a1", "font-weight", "700"); },
-  function() { return hasCorrectStyleValue("a1", "font-size", `24px`); },
-]
+    function () {
+        return elementIsCorrectTag("h11", "h1");
+    },
+    function () {
+        return hasCorrectStyleValue("h11", "text-align", `center`);
+    },
+    function () {
+        return hasCorrectStyleValue(
+            "h11",
+            "background-color",
+            "rgb(47, 79, 79)"
+        );
+    },
+    function () {
+        return hasCorrectStyleValue("h11", "color", "rgb(255, 255, 255)");
+    },
+    function () {
+        return hasCorrectStyleValue("a1", "color", "rgb(255, 0, 0)");
+    },
+    function () {
+        return elHasCorrectStyleValue(
+            document.getElementById("p1"),
+            "p1",
+            "font-family",
+            `Arial, Helvetica, sans-serif`
+        );
+    },
+    function () {
+        return hasCorrectStyleValue("a1", "font-weight", "700");
+    },
+    function () {
+        return hasCorrectStyleValue("a1", "font-size", `24px`);
+    },
+];
 
-let exerciseBase = new Exercise(exerciseID, instructions, tips, validationFuncs);
+let exerciseBase = new Exercise(
+    exerciseID,
+    instructions,
+    tips,
+    validationFuncs
+);
 window.onload = exerciseBase.init();
